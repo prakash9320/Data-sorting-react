d1bf9ebf729f16aab482462b668985e94773cba4
prakash9320 <prakashshukla275@gmail.com>
2021-10-06 11:54:33 +0530

    import useReducer

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import Resturent from './Comonents/Basic/resturent';
 import UseState from './Comonents/hooks/useState';
 import UseEffect from './Comonents/hooks/useEffect';
+import UseReducer from  './Comonents/hooks/useReduce';
  const App = () => {
   return (
     <div>
-       <UseEffect/>
+       <UseReducer/>
     </div>
   )
 }
