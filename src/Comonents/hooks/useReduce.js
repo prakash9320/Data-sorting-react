baeb43f39c54238a6c8efa996480c17ff381fd6e
prakash9320 <prakashshukla275@gmail.com>
2021-10-06 13:11:31 +0530

    import and implement useReducer increment and decrement Value

--- src/Comonents/hooks/useReduce.js
+++ src/Comonents/hooks/useReduce.js
@@ -1,15 +1,27 @@
-import React,{useState} from "react";
+import React,{useState,useReducer} from "react";
 import "./style.css";
 
-const useReducer = () => {
-  // const initialData = 15;
-  const [myNum, setMyNum] = useState(0); 
+const Reducer = (state,action)=>{
+    if(action.type === "Increment"){
+      state = state + 1;
+    }
+    if(state > 0 && action.type === "Decrement"){
+      state = state - 1;
+    }
+    return state;
+};
+
+const UseReducer = () => {
+    const initialDate = 15;
+   const [state,dispatch] = useReducer(Reducer,initialDate)
+  
+  // const [myNum, setMyNum] = useState(0); 
 
   return (
     <>
       <div className="center_div">
-        <p>{myNum}</p>
-        <div class="button2" onClick={() => setMyNum(myNum + 1)}>
+        <p>{state}</p>
+        <div class="button2" onClick={() =>dispatch({type:"Increment"})}>
           <span></span>
           <span></span>
           <span></span>
@@ -18,7 +30,7 @@ const useReducer = () => {
         </div>
         <div
           class="button2"
-          onClick={() => (myNum > 0 ? setMyNum(myNum - 1) : setMyNum(0))}>
+          onClick={() =>dispatch({type:"Decrement"})}>
           <span></span>
           <span></span>
           <span></span>
@@ -30,4 +42,4 @@ const useReducer = () => {
   );
 };
 
-export default useReducer;
+export default UseReducer;
